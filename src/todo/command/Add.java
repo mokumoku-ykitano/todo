8719011368eb77678586a0bf21d0f9d0f6af08da
ykitano <csd.wg.ykitano@gmail.com>
2017-08-26 09:33:45 +0900

    ,区切りで複数追加できるよう修正

--- src/todo/command/Add.java
+++ src/todo/command/Add.java
@@ -7,34 +7,39 @@ import todo.TodoLogic;
 import todo.dto.json.Todo;
 import todo.exception.TodoException;
 import todo.util.MessageUtil;
+import todo.util.StringUtil;
 
 public class Add extends Command {
 
-	private String todoTitle;
+	private String todoTitles;
 
 	@Override
 	public void setArguments(String[] args) {
 		if (args == null || args.length == 0) {
 			throw new IllegalArgumentException(MessageUtil.getMessage("error.command.add.argument"));
 		}
-		todoTitle = args[0];
+		todoTitles = args[0];
 	}
 
 	@Override
 	public void execute() throws TodoException {
 		try {
 			List<Todo> todoList = TodoLogic.loadTodoList();
-			todoList.add(new Todo(todoTitle));
+			for (String todoTitle : todoTitles.split(",")) {
+				if (StringUtil.isNotEmpty(todoTitle)) {
+					todoList.add(new Todo(todoTitle));
+				}
+			}
 
 			TodoLogic.writeTodoList(todoList);
 
-			System.out.println(MessageUtil.getMessage("info.command.add.finish", todoTitle));
-			
+			System.out.println(MessageUtil.getMessage("info.command.add.finish", todoTitles));
+
 			Command listCommand = new todo.command.List();
 			listCommand.execute();
 
 		} catch (IOException e) {
-			throw new TodoException(e, "error.command.add.list", todoTitle);
+			throw new TodoException(e, "error.command.add.list", todoTitles);
 		}
 	}
 }
