b38ef4128d5aadb8fc3d269f4437ff0a8938e23c
ykitano <csd.wg.ykitano@gmail.com>
2017-09-09 11:04:35 +0900

    終了時に実行中のTodoがある場合は停止する

--- src/todo/command/Quit.java
+++ src/todo/command/Quit.java
@@ -1,5 +1,6 @@
 package todo.command;
 
+import todo.TodoControl;
 import todo.exception.TodoException;
 import todo.util.MessageUtil;
 
@@ -7,6 +8,12 @@ public class Quit extends Command {
 
 	@Override
 	public void execute() throws TodoException {
+		// 実行中のtodoがある場合、stopする
+		if (TodoControl.isExecutingTodo()) {
+			Command stopCommand = new Stop();
+			stopCommand.execute();
+			stopCommand.showMessage();
+		}
 		System.out.println(MessageUtil.getMessage("info.command.quit"));
 	}
 
